7cf6a5bc9a654177e19b0df470ce7a8d08f5c6e7
Tunna <tmajmundar@gmail.com>
2016-08-09 12:45:11 -0700

    Final revisions

--- shopping_list.py
+++ shopping_list.py
@@ -9,48 +9,48 @@ def add_shopping(add_item):
 	return my_list
 
 
-# def remove_shopping(delete_item):
-# 	if delete_item.lower() not in my_list:
-# 		print "This item %s not in list therefore cannot be removed." % (delete_item)
-# 	else:
-# 		my_list.remove(delete_item)
-# 	my_list.sort()
-# 	return my_list	
+def remove_shopping(delete_item):
+	if delete_item.lower() not in my_list:
+		print "This item %s not in list therefore cannot be removed." % (delete_item)
+	else:
+		my_list.remove(delete_item)
+	my_list.sort()
+	return my_list	
 
 
-def menu_function(choice):
+def menu_function():
+	print "Please select one of the options below."
 	print "0 - Main Menu"
 	print "1 - Show current list."
 	print "2 - Add an item to your shopping list."
-	return choice
+	print "3 - Remove an item from shopping list."
+	print "Type exit to exit."
+	option = raw_input("Enter your selection: ")
+	return option
 	
 def main(): 
 	
-	prompt = raw_input("Please select one of the options below.")
-	print menu_function(prompt)
 
-	menu = []
-	option = 0
+	
+
+	
 	while (True):
-		print "Type exit to exit"
-		if(option == "exit"):
-			print menu_function(choice)
-		elif option == 0:
-			print menu_function(choice)
-		elif option == 1:
-			print "Your items are", my_list
-		elif option == 2:
-			print add_shopping(add)
-		else:
+		option = menu_function()
+		if option == "exit":
 			break
-
-		option = option + 1	
-
-	add = raw_input("What would you like to add to your grocery list?")
-	print add_shopping(add)
-
-	# remove = raw_input('What would you like to remove from your grocery list?')
-	# print remove_shopping(remove)
+		elif option == "0":
+			pass
+		elif option == "1":
+			print "Your items are", my_list
+		elif option == "2":
+			add = raw_input("What would you like to add to your grocery list?")
+			print add_shopping(add)
+		elif option == "3":
+			remove = raw_input('What would you like to remove from your grocery list?')
+			print remove_shopping(remove)
+		else:
+			print "Invalid option. Please try again."
+			
 
 if __name__ == '__main__':
     main()
